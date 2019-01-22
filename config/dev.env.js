1caafd297d8b0165287a3b62d50a94b013ce43b5
liangyongsen <18922431411@189.cn>
2019-01-22 19:47:03 +0800

    增加列表功能

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  BASE_API: '"https://www.easy-mock.com/mock/5c46e36969e1dd2d0053e4ef/tensqure"',
 })
---
+++ src/views/table/gathering.vue
@@ -0,0 +1,53 @@
+<template>
+ <el-table
+    :data="list"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="id"
+      label="活动ID"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="活动名称"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="sponsor"
+      label="主办方">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="活动地址">
+    </el-table-column>
+    <el-table-column
+      prop="starttime"
+      label="开始日期">
+    </el-table-column>
+    <el-table-column
+      prop="endtime"
+      label="结束时间">
+    </el-table-column>
+  </el-table>
+</template>
+<script>
+import gatheringApi from "@/api/gathering"
+export default {
+    data() {
+        return {
+            list:[]
+        }
+    },
+    created() {
+        this.fetchData()
+    },
+    methods: {
+        fetchData() {
+            gatheringApi.getList().then(response =>{
+                this.list = response.data
+            })
+        }
+    }
+}
+</script>
